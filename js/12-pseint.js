98a0bb2198197bd4c798fa1e4b84e1eb00668ce3
Gonzalo Diaz <sergiogonzalodiazlondero@gmail.com>
2025-06-12 15:10:37 -0300

    pseint 1

---
+++ js/12-pseint.js
@@ -0,0 +1,18 @@
+//comentario una sola linea
+
+/*
+multiples lineas
+*/
+
+//crear una variable (let - var - const)
+
+let numero1; 
+
+let anio = 2025
+
+//mostrar el valor de las variables 
+console.log("Hola mundo")
+console.log(numero1, anio)
+
+document.writeln("<h1>Practica js comision: " + comision + "</h1>")
+document.writeln("<p>Contenido de la variable año: " + anio + "</p>")
