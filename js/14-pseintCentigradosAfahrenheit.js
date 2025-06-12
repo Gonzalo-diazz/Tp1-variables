23c152b7d3466c3c6605f25fc1c10010c67b4774
Gonzalo Diaz <sergiogonzalodiazlondero@gmail.com>
2025-06-12 15:15:17 -0300

    pseint centigrados a fahremheit

---
+++ js/14-pseintCentigradosAfahrenheit.js
@@ -0,0 +1,8 @@
+document.writeln("<h1>Ejercicio 1 </h1>")
+const gradoCentigrado = parseInt(prompt("ingresa los grados centigrados"))
+const gradoFahenheit = (gradoCentigrado * 9/5) + 32
+
+document.writeln("Los grados centigrados: " + gradoCentigrado + "C, equivalente a: " + gradoFahenheit + "F")
+
+alert("Los grados centigrados: " + gradoCentigrado + "C, equivalente a: " + gradoFahenheit + "F")
+
