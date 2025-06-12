bd0f6db2b5822a050e4c0dd5854215222303561c
Gonzalo Diaz <sergiogonzalodiazlondero@gmail.com>
2025-06-12 15:04:30 -0300

    punto 11

--- js/11-ejercicio.js
+++ js/11-ejercicio.js
@@ -1,6 +1,5 @@
-// 10.- Escribe un programa que pida un número y nos diga si es divisible por 2, 3, 5 o 7
-//  (sólo hay que comprobar si lo es por uno de los cuatro)
-
+// 11.- Añadir al ejercicio anterior que nos diga por cuál de los cuatro es divisible 
+// (hay que decir todos por los que es divisible)
 
 
 const n = parseInt(prompt("Ingrese un numero:"))
@@ -10,11 +9,11 @@ if (n % 2 === 0){
   alert(`El ${n} es divisible por 2`)
 
 }
-else if (n % 3 === 0){
+if (n % 3 === 0){
   alert(`El ${n} es divisible por 3`)
 
 } 
-else if (n % 5 === 0){
+if (n % 5 === 0){
   alert(`El ${n} es divisible por 5`)
 
 }
