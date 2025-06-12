3e1f23903b6f0794b51050d9e98e0cba0e751a8b
Gonzalo Diaz <sergiogonzalodiazlondero@gmail.com>
2025-06-12 14:06:34 -0300

    punto 8

---
+++ js/08-ejercicio.js
@@ -0,0 +1,10 @@
+//8.- Escribe un programa que pida un número y diga si es divisible por 2
+
+const n = parseInt(prompt("Ingrese un numero:"))
+
+if(n % 2 === 0){
+    alert(`El ${n} es divisible en 2`)
+}
+else{
+     alert(`El ${n} no es divisible en 2`)
+}
